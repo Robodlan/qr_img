add43ca05a0dac9395c507d8c2f9f49f45e8772e
Robodlan <azuldevs@zohomail.com>
2024-07-17 20:05:09 -0400

    add png

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 import { input } from '@inquirer/prompts';
+import qr  from 'qr-image';
+import fs from 'fs';
+import { writeFile } from 'fs';
 
-const answer = await input({ message: 'Enter your url' });
+const answer = await input({ 
+    message: 'Enter your url' 
+});
+ 
+var qr_svg = qr.image(answer);
+qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+writeFile('message.txt', answer, (err)=> {
+    if (err) throw err;
+    console.log('Done!');
+})
+
 
